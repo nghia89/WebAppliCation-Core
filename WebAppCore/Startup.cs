9d5acb575431af0e9ba135762d1e7b6602752fd0
DESKTOP-O9PNQI9\thanh <hangnghia11089@gmail.com>
2018-06-11 14:08:10 +0700

    Dựng cấu trúc Admin

--- WebAppCore/Startup.cs
+++ WebAppCore/Startup.cs
@@ -97,6 +97,11 @@ namespace WebAppCore
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "areasRouter",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
             });
            
         }
