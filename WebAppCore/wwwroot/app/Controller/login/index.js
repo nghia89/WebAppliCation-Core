d929347af287ffe064697667d70036cf293fa426
DESKTOP-O9PNQI9\thanh <hangnghia11089@gmail.com>
2018-07-03 23:00:25 +0700

    Update Bai19-20

---
+++ WebAppCore/wwwroot/app/Controller/login/index.js
@@ -0,0 +1,63 @@
+﻿var loginController = function () {
+    this.initialize = function () {
+        registerEvents();
+    }
+
+    var registerEvents = function () {
+
+        $('#frmLogin').validate({
+            rules: {
+                userName: {
+                    required: true,
+                    minlength: 5
+
+                },
+                password: {
+                    required: true,
+                    minlength: 6
+                }
+
+            },
+            messages: {
+                userName: {
+                    required: "Bạn phải điền tên UserName",
+                    minlength: "Tên phải chứa hơn 6 ký tự"
+
+                },
+                password: {
+                    required: "Bạn phải điền tên Password",
+                    minlength: "Password phải chứa hơn 6 ký tự"
+                }
+
+            }
+        });
+
+        $('#btnLogin').on('click', function (e) {
+            e.preventDefault();
+            if ($('#frmLogin').valid()) {
+                var user = $('#txtUserName').val();
+                var password = $('#txtPassword').val();
+                login(user, password);
+            }
+        });
+    }
+    var login = function (user, pass) {
+        $.ajax({
+            type: 'POST',
+            data: {
+                UserName: user,
+                PassWord: pass
+            },
+            dataType: 'json',
+            url: '/Admin/Login/Authen',
+            success: function (res) {
+                if (res.Success) {
+                    window.location.href = "/Admin/Home/Index"
+                }
+                else {
+                    structures.notify('Đăng nhập không đúng', 'error')
+                }
+            }
+        })
+    }
+}
