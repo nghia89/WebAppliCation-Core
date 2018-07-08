010ef59d3f2e27c79817eddc72e085f5591b7ee8
DESKTOP-O9PNQI9\thanh <hangnghia11089@gmail.com>
2018-07-08 23:20:50 +0700

    Bài  24. Phân trang danh sách sản phẩm bằng Ajax

--- WebAppCore.Application/Interfaces/IProductService.cs
+++ WebAppCore.Application/Interfaces/IProductService.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using WebAppCore.Application.ViewModels.Product;
+using WebAppCore.Utilities.Dtos;
 
 namespace WebAppCore.Application.Interfaces
 {
     public interface IProductService : IDisposable
     {      
         List<ProductViewModel> GetAll();
+        PagedResult<ProductViewModel> GetAllPaging(int? categoryId, string keyword, int page, int pageSize);
     }
 }
--- WebAppCore/wwwroot/app/Controller/product/Index.js
+++ WebAppCore/wwwroot/app/Controller/product/Index.js
@@ -1,20 +1,32 @@
 ﻿var productController=function()
 {
     this.initialize = function () {
-        loadDate();
+        loadData();
+        registerEvents();
     }
     function registerEvents() {
-
+        //todo: binding events to controls
+        $('#ddlShowPage').on('change', function () {
+            structures.configs.pageSize = $(this).val();
+            structures.configs.pageIndex = 1;
+            loadData(true);
+        });
     }
-    function loadDate() {
+    function loadData(isPageChanged) {
         var template = $('#table-template').html();
         var render = "";
         $.ajax({
             type: 'GET',
-            url: '/admin/product/GetAll',
+            data: {
+                categoryId: null,
+                keyword: $('#txtKeyword').val(),
+                page: structures.configs.pageIndex,
+                pageSize: structures.configs.pageSize
+            },
+            url: '/admin/product/GetAllPaging',
             dataType: 'JSON',
-            success: function (res) {
-                $.each(res, function (i, item) {
+            success: function (response) {
+                $.each(response.Results, function (i, item) {
                     render += Mustache.render(template, {
                         Id: item.Id,
                         Name: item.Name,
@@ -24,15 +36,41 @@
                         CreatedDate: structures.dateTimeFormatJson(item.DateCreated),
                         Status: structures.getStatus(item.Status)
                     });
+                    $('#lblTotalRecords').text(response.RowCount);
                     if (render != '') {
                         $('#tbl-content').html(render);
                     }
+                    wrapPaging(response.RowCount, function () {
+                        loadData();
+                    }, isPageChanged);
                 });
             },
             error: function (status) {
                 console.log(status);
                 structures.notify('Không thể tải dữ liệu','error')
             }
-        })
+        });
+    }
+    function wrapPaging(recordCount, callBack, changePageSize) {
+        var totalsize = Math.ceil(recordCount / structures.configs.pageSize);
+        //Unbind pagination if it existed or click change pagesize
+        if ($('#paginationUL a').length === 0 || changePageSize === true) {
+            $('#paginationUL').empty();
+            $('#paginationUL').removeData("twbs-pagination");
+            $('#paginationUL').unbind("page");
+        }
+        //Bind Pagination Event
+        $('#paginationUL').twbsPagination({
+            totalPages: totalsize,
+            visiblePages: 7,
+            first: 'Đầu',
+            prev: 'Trước',
+            next: 'Tiếp',
+            last: 'Cuối',
+            onPageClick: function (event, p) {
+                structures.configs.pageIndex = p;
+                setTimeout(callBack(), 200);
+            }
+        });
     }
 }
