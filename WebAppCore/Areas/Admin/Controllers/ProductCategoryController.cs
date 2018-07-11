821d510b4de8bfc70762a01c96be035f21394031
DESKTOP-O9PNQI9\thanh <hangnghia11089@gmail.com>
2018-07-11 19:43:54 +0700

    Bài 26-27. hiển thị danh và Sắp xếp danh mục bằng kéo thả trên Treeview

---
+++ WebAppCore/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppCore.Application.Interfaces;
+
+namespace WebAppCore.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController 
+    {
+        IProductCategoryService _productCategoryService;
+        public ProductCategoryController( IProductCategoryService productCategoryService)
+        {
+          
+            _productCategoryService = productCategoryService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Ajax Api
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var model = _productCategoryService.GetAll();
+            return new OkObjectResult(model);
+        }
+        [HttpPost]
+        public IActionResult UpdateParentId(int sourceId, int targetId, Dictionary<int, int> items)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);
+            }
+            else
+            {
+                if (sourceId == targetId)
+                {
+                    return new BadRequestResult();
+                }
+                else
+                {
+                    _productCategoryService.UpdateParentId(sourceId, targetId, items);
+                    _productCategoryService.Save();
+                    return new OkResult();
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult ReOrder(int sourceId, int targetId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);
+            }
+            else
+            {
+                if (sourceId == targetId)
+                {
+                    return new BadRequestResult();
+                }
+                else
+                {
+                    _productCategoryService.ReOrder(sourceId, targetId);
+                    _productCategoryService.Save();
+                    return new OkResult();
+                }
+            }
+        }
+
+    }
+}
---
+++ WebAppCore/wwwroot/app/Controller/product-category/index.js
@@ -0,0 +1,81 @@
+﻿var productCategoryController = function () {
+    this.initialize = function () {
+        loaData();
+    }
+
+    function loaData() {
+        $.ajax({
+            url: '/Admin/ProductCategory/GetAll',
+            dataType: 'json',
+            success: function (response) {
+                var data = [];
+                $.each(response, function (i, item) {
+                    data.push({
+                        id: item.Id,
+                        text: item.Name,
+                        parentId: item.ParentId,
+                        sortOrder: item.SortOrder
+                    });
+                });
+                //structures.unflattern chuy?n m?ng ph?ng thành cây
+                var treeArr = structures.unflattern(data);
+                treeArr.sort(function (a, b) {
+                    return a.sortOrder - b.sortOrder;
+                });
+                //var $tree = $('#treeProductCategory');
+
+                $('#treeProductCategory').tree({
+                    data: treeArr,
+                    dnd: true, //cho phép kéo th?
+                    //taget là v? trí
+                    //source là ngu?n
+                    //point là ki?u thao tác
+                    onDrop: function (target, source, point) {
+                        console.log(target);
+                        console.log(source);
+                        console.log(point);
+                        var targetNode = $(this).tree('getNode', target);
+                        if (point === 'append') {
+                            var children = [];
+                            $.each(targetNode.children, function (i, item) {
+                                children.push({
+                                    key: item.id,
+                                    value: i
+                                });
+                            });
+
+                            //Update to database
+                            $.ajax({
+                                url: '/Admin/ProductCategory/UpdateParentId',
+                                type: 'post',
+                                dataType: 'json',
+                                data: {
+                                    sourceId: source.id,
+                                    targetId: targetNode.id,
+                                    items: children
+                                },
+                                success: function (res) {
+                                    loadData();
+                                }
+                            });
+                        }
+                        else if (point === 'top' || point === 'bottom') {
+                            $.ajax({
+                                url: '/Admin/ProductCategory/ReOrder',
+                                type: 'post',
+                                dataType: 'json',
+                                data: {
+                                    sourceId: source.id,
+                                    targetId: targetNode.id
+                                },
+                                success: function (res) {
+                                    loadData();
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
