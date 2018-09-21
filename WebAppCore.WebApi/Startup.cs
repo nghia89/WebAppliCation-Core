ec7b444ad9c94b859dc803bf213568a82bbc4658
DESKTOP-O9PNQI9\thanh <hangnghia11089@gmail.com>
2018-09-21 22:19:43 +0700

    Bai65: Tạo Project Web API cho Mobile

---
+++ WebAppCore.WebApi/Startup.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
+using WebAppCore.Application.Implementation;
+using WebAppCore.Application.Interfaces;
+using WebAppCore.Data.EF;
+using WebAppCore.Infrastructure.Interfaces;
+
+namespace WebAppCore.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                   options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                       b => b.MigrationsAssembly("WebAppCore.Data.EF")));
+
+            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+            services.AddAutoMapper();
+
+            services.AddSingleton(Mapper.Configuration);
+            services.AddScoped<IMapper>(sp => new Mapper(sp.GetRequiredService<AutoMapper.IConfigurationProvider>(), sp.GetService));
+            services.AddTransient(typeof(IUnitOfWork), typeof(EFUnitOfWork));
+            services.AddTransient(typeof(IRepository<,>), typeof(EFRepository<,>));
+
+            services.AddTransient<IProductCategoryService, ProductCategoryService>();
+
+            services.AddMvc().
+                AddJsonOptions(options =>
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "WebApp Project",
+                    Description = "WebApp API Swagger surface",
+                    Contact = new Contact { Name = "nghia", Email = "hangnghia11089@gmail.com", Url = "http://www.tedu.com.vn" },
+                    License = new License { Name = "MIT", Url = "https://github.com/nghia89/WebAppliCation-Core" }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "Project API v1.1");
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
